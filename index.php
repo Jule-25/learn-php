537f846fb96cd90a72a3fcea33e882e884897e73
Jule Sall <souleymanesall985@gmail.com>
2025-08-01 20:43:32 +0000

    embedding php code inside html tags

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo 'Hello World!';
+//'Hello World!';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Time With PHP</title>
+</head>
+
+<body>
+    <h1>
+        <?php echo 'Hello World!'; ?>
+    </h1>
+
+</body>
+
+</html>
