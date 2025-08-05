9623e62585bbfff9d00e6595a2ce503ef61be55a
Jule Sall <souleymanesall985@gmail.com>
2025-08-05 10:42:48 +0000

    Getting started with our Pizza Shop project by making the header and footer templates

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php
-// include and require
 
-//include('names.php');
-require('names.php');
-
-echo 'end of php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First Time With PHP</title>
-</head>
+<?php include("templates/header.php"); ?>
 
-<body>
+<?php include("templates/footer.php") ?>
 
-</body>
 
 </html>
